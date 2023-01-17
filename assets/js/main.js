fcd15dcca5936ac3b1c3bb76604a5c27f2fcb846
unknown <guimaraesgn30@gmail.com>
2023-01-17 19:34:24 -0300

    criação de caracteristicas do pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,25 @@
 
 
+function convertPokemonToLi(pokemon){
+    return` 
+    <li class = "pokemon ${pokemon.type}">
+        <span class = "number">${pokemon.number}</span>
+        <span class = "name">${pokemon.name}</span>
 
-const offset= 0
-const limit = 10
-const url='https://pokeapi.co/api/v2/pokemon?offset=0&limite=10'
+        <div class= "detail">
+        <ol class = "types">
+              ${pokemon.types.map((type) => `<li class = "type ${type}">${type}</li>`).join('')}
+        </ol>
+        <img src="${pokemon.photo}" 
+        alt="${pokemon.name}">
+    </div>
+</li>
+`
+}
 
+const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
+pokeApi.getPokemons().then((pokemons = []) => {
+       const newHtml = pokemons.map(convertPokemonToLi).join('')
+       pokemonList.innerHTML = newHtml
+      })
